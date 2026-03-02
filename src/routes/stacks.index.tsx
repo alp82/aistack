e35ab33e0377e1b6da4843fa6d5d247b86f87bf6
Alper Ortac <alportac@gmail.com>
2026-03-02 16:56:21 +0100

    better stack grid

--- src/routes/stacks.index.tsx
+++ src/routes/stacks.index.tsx
@@ -179,7 +179,7 @@ function BrowseStacksPage() {
 							No stacks match these filters.
 						</div>
 					) : (
-						<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+						<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
 							{filteredStacks.map((stack) => (
 								<StackArtifactCard key={stack._id} stack={stack} />
 							))}
