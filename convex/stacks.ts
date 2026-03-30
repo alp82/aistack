1653f89265b47c6753ed3056dc85c5c89669944e
Alper Ortac <alportac@gmail.com>
2026-03-30 21:35:21 +0200

    refactor: deprecate content field in InstructionItemValidator and make files optional

--- convex/stacks.ts
+++ convex/stacks.ts
@@ -93,7 +93,9 @@ const InstructionItemValidator = v.object({
   type: v.string(),
   name: v.string(),
   description: v.optional(v.string()),
-  files: v.array(InstructionFileValidator),
+  /** @deprecated Migrate old instructions to use files[] */
+  content: v.optional(v.string()),
+  files: v.optional(v.array(InstructionFileValidator)),
 })
 
 const MoneyValidator = v.object({
